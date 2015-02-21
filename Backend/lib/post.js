9670f2d9cb906b1ba7aec6611951f08439c2b9ff
Edward Digby <edward.jdigby@tiscali.co.uk>
2015-02-21 14:55:42 +0000

    Commit

--- Backend/lib/post.js
+++ Backend/lib/post.js
@@ -13,21 +13,25 @@ request("http://www.vam.ac.uk/api/json/museumobject/", function(error, response,
  	 
 	body = JSON.parse(body)
 
-  	for (i=0; i <= 10; i++){
+  	for (i=0; i <= 11; i++){
   		body1 = body.records
 //  		x = randomInt(0, body1.length)
 		x = i
-		
+
+		if (body1[x].fields.location != "In Storage"){
+
   		var object = {
   			"Name": body1[x].fields.object,
+  			"Date": body1[x].fields.year_start,
   			"Location": body1[x].fields.location,
   			"Latitude": body1[x].fields.latitude,
   			"Longitude": body1[x].fields.longitude,
-  			"Place": body1[x].fields.Place,
-  			"pk": body1[x].pk,
-  			"image_id": body1[x].fields.primary_image_id
-  		}
-  		finalarray.push(object)
+  			"Place": body1[x].fields.place,
+  			"pk": body1[x].fields.object_number,
+  			"image_id": body1[x].fields.primary_image_id}
+
+  		  		finalarray.push(object)
+		}
   	}
 });
 return finalarray
