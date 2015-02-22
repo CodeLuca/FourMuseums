38b5aae0311cb6db9959291e2125eafd7046f4b0
James Brooks <james.brooks2@btinternet.com>
2015-02-22 17:30:04 +0000

    Fixed duplicates bug hopefully

--- Server/lib/post.js
+++ Server/lib/post.js
@@ -4,10 +4,8 @@ function randomInt (low, high) {
     return Math.floor(Math.random() * (high - low) + low);
 }
 
-var dataArray = []; 
-var qrArray = []; 
-var ids = [];
-var names = [];
+var dataArray = [];
+var qrArray = [];
 
 module.exports = {
  post:function(){
@@ -17,17 +15,14 @@ request("http://www.vam.ac.uk/api/json/museumobject/", function(error, response,
 
     for (i=0; i <= 10; i++){
       body1 = body.records
-      x = randomInt(0, body1.length)
-      ids[i] = body1[x].pk;
-      names[i] = body1[x].fields.object;
       var object = {
-        "Name": body1[x].fields.object,
-        "Location": body1[x].fields.location,
-        "Latitude": body1[x].fields.latitude,
-        "Longitude": body1[x].fields.longitude,
-        "Place": body1[x].fields.Place,
-        "pk": body1[x].pk,
-        "image_id": body1[x].fields.primary_image_id
+        "Name": body1[i].fields.object,
+        "Location": body1[i].fields.location,
+        "Latitude": body1[i].fields.latitude,
+        "Longitude": body1[i].fields.longitude,
+        "Place": body1[i].fields.Place,
+        "pk": body1[i].pk,
+        "image_id": body1[i].fields.primary_image_id
       }
       dataArray.push(object)
     }
